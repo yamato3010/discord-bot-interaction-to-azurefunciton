31b385fa0284fa1d71580fbd09e041a3633ee175
yamato3010 <yama3to10yama3@gmail.com>
2025-05-19 01:50:36 +0900

    refactor: ♻️ gitignoreを修正

--- dist/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const functions_1 = require("@azure/functions");
-functions_1.app.setup({
-    enableHttpStream: true,
-});
-//# sourceMappingURL=index.js.map
